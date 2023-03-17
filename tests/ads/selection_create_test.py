edd64f9f464eb6ca924c1a4a57fcb7e1a1052ad8
Nadiabona <Nadinkotikoff@gmail.com>
2023-03-17 20:17:07 +0300

    HW_31_Golubeva_ready

--- tests/ads/selection_create_test.py
+++ tests/ads/selection_create_test.py
@@ -1,25 +1,26 @@
-# import pytest
-# from rest_framework import status
+import pytest
+from rest_framework import status
+
+from tests.factories import AdsFactory
 
 
-# @pytest.mark.django_db
-# def test_selection_create(client, user, access_token):
-#     data = {
-#         "author_id": user.pk,
-#         "name": "tableVeryGood",
-#         "price": 200
-#     }
-#
-#     expected_data = {
-#         "id": 1, #тестовая база всегда пустая
-#         "is_published": False,
-#         "name": "tableVeryGood",
-#         "price": 200,
-#         "description": None,
-#         "image": None,
-#         "author_id": user.pk,
-#     }
+@pytest.mark.django_db
+def test_selection_create(client, user_access_token):
+    user, access_token = user_access_token
+    ads_list = AdsFactory.create_batch(5)
+    data = {
+        "name": "my_selection",
+        "items" :[ads.pk for ads in ads_list]
+    }
 
-    # response = client.post("/selection/", data=data, HTTP_AUTHORIZATION=f"Bearer {access_token}")
-    # assert response.status_code==status.HTTP_201_CREATED
-    # assert response.data == expected_data
+    expected_data = {
+        "id": 1, #тестовая база всегда пустая
+        "owner": user.username,
+        "name": "my_selection",
+        "items" :[ads.pk for ads in ads_list]
+
+    }
+
+    response = client.post("/selection/", data=data, HTTP_AUTHORIZATION=f"Bearer {access_token}")
+    assert response.status_code==status.HTTP_201_CREATED
+    assert response.data == expected_data
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -11,3 +11,15 @@ def access_token(client, django_user_model):
     response = client.post("/user/token/", data = {"username":username, "password":password}, format='json')
 
     return response.data.get("access")
+
+@pytest.fixture()
+@pytest.mark.django_db
+def user_access_token(client, django_user_model):
+    username="test_username"
+    password = "test_password"
+
+    new_user = django_user_model.objects.create_user(username=username, password = password, role = "admin")
+
+    response = client.post("/user/token/", data = {"username":username, "password":password}, format='json')
+
+    return new_user, response.data.get("access")
